5457c8d46293e9787a9ed736f39ad4e409446c07
Robert Koech <rk214@duke.edu>
2021-04-12 22:51:46 -0600

    add

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+
 const mongoose = require("mongoose");
+
 //jshint esversion:6
 
 const express = require("express");
@@ -143,6 +145,12 @@ app.post("/compose",function(req,res){
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function() {
+  console.log("Server started");
 });
